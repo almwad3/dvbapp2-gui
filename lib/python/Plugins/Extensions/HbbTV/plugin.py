905fbdf6eeacfcf29ef29f44e4e56004a59907a6
a4tech <Dev1@dev>
2012-12-17 19:48:24 +0100

    merge fix

--- lib/python/Plugins/Extensions/HbbTV/plugin.py
+++ lib/python/Plugins/Extensions/HbbTV/plugin.py
@@ -2153,7 +2153,7 @@ def Plugins(path, **kwargs):
 	l.append(PluginDescriptor(where=PluginDescriptor.WHERE_SESSIONSTART, needsRestart=True, fnc=session_start_main, weight=-10))
 	l.append(PluginDescriptor(name=_("HbbTV Applications"), where=PluginDescriptor.WHERE_EXTENSIONSMENU, needsRestart=True, fnc=plugin_extension_start_application))
 	l.append(PluginDescriptor(name=_("Browser Start/Stop"), where=PluginDescriptor.WHERE_EXTENSIONSMENU, needsRestart=True, fnc=plugin_extension_browser_config))
-	l.append(PluginDescriptor(name=_("Opera Web Browser"), description=_("start opera web browser"), where=PluginDescriptor.WHERE_PLUGINMENU, needsRestart=True, fnc=plugin_start_main))
+	l.append(PluginDescriptor(name=_("Web Browser"), description=_("start web browser"), where=PluginDescriptor.WHERE_PLUGINMENU, needsRestart=True, fnc=plugin_start_main))
 
 	return l
 
