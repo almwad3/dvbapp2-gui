406ebfacdce8f3dbc22ee340ae7b9cd20641c0e3
a4tech <Dev1@dev>
2013-04-23 16:13:48 +0200

    fixed typo

--- lib/python/EGAMI/EGAMI_tools.py
+++ lib/python/EGAMI/EGAMI_tools.py
@@ -18,7 +18,7 @@ def checkkernel():
 	if not fileExists("/media/usb"):
 		os.system("mkdir /media/usb")
 	if (os.path.isfile("/proc/stb/info/boxtype") and os.path.isfile("/proc/stb/info/version")): 
-		if open("/proc/stb/info/boxtype").read().startswith("ini-10" or open("/proc/stb/info/boxtype").read().strip() == "ini-3000" or open("/proc/stb/info/boxtype").read().startswith("ini-50") or open("/proc/stb/info/boxtype").read().startswith("ini-70"):
+		if open("/proc/stb/info/boxtype").read().startswith("ini-10") or open("/proc/stb/info/boxtype").read().strip() == "ini-3000" or open("/proc/stb/info/boxtype").read().startswith("ini-50") or open("/proc/stb/info/boxtype").read().startswith("ini-70"):
 			if (about.getKernelVersionString()=="3.6.0"):
 				mycheck = 1
 	else:
