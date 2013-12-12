f2da80a5616752a721ebdf235babb6037de36abd
a4tech <darezik@gmail.com>
2013-12-12 19:23:23 +0100

    Fixed GSOD

--- lib/python/Screens/Timershift.py
+++ lib/python/Screens/Timershift.py
@@ -41,7 +41,7 @@ class SetupSummary(Screen):
 
 class TimeshiftSettings(Screen,ConfigListScreen):
 	def removeNotifier(self):
-		config.usage.setup_level.notifiers.remove(self.levelChanged)
+		self.onNotifiers.remove(self.levelChanged)
 
 	def levelChanged(self, configElement):
 		list = []
@@ -70,6 +70,7 @@ class TimeshiftSettings(Screen,ConfigListScreen):
 		self["description"] = Label(_(""))
 
 		self.onChangedEntry = [ ]
+		self.onNotifiers = [ ]
 		self.setup = "timeshift"
 		list = []
 		ConfigListScreen.__init__(self, list, session = session, on_change = self.changedEntry)
@@ -293,11 +294,11 @@ class TimeshiftSettings(Screen,ConfigListScreen):
 			if x.tag == 'item':
 				item_level = int(x.get("level", 0))
 
-				if not self.levelChanged in config.usage.setup_level.notifiers:
-					config.usage.setup_level.notifiers.append(self.levelChanged)
+				if not self.onNotifiers:
+					self.onNotifiers.append(self.levelChanged)
 					self.onClose.append(self.removeNotifier)
 
-				if item_level > config.usage.setup_level.index:
+				if item_level > self.onNotifiers.index:
 					continue
 
 				requires = x.get("requires")
